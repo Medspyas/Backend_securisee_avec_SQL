faf031ac266d8ac95c91d784d9bc7b74f13c49b9
assakali <assakali.mehdi@gmail.com>
2025-04-14 11:51:35 +0200

    Ajout de tests unitaires sur la création de token et lecture

---
+++ tests_unitaires/test_token.py
@@ -0,0 +1,50 @@
+import unittest
+from unittest.mock import patch, MagicMock, mock_open
+from token_user import create_token, read_token
+from models import User
+
+
+class TestCreateToken(unittest.TestCase):
+    @patch('builtins.open', new_callable=mock_open)
+    @patch('jwt.encode')
+    def test_create_token(self, mock_jwt_encode,  mock_open_file):
+        user = MagicMock()
+        user.id = 1
+        user.email = 'test1@mail.com'
+        user.role.value = 'gestion'
+
+        mock_jwt_encode.return_value = "FAKE_JWT_TOKEN"
+
+        create_token(user)
+
+        mock_open_file.assert_called_once_with('.jwt_token', 'w')
+
+        mock_open_file().write.assert_called_once_with("FAKE_JWT_TOKEN")
+
+    @patch('builtins.open', new_callable=mock_open, read_data="FAKE_JWT_TOKEN")
+    @patch('os.path.exists', return_value=True)
+    @patch('jwt.decode')
+    def test_read_token(self, mock_jwt_decode, mock_exists, mock_open_file):
+        mock_jwt_decode.return_value = {
+            "user_id" : 1,
+            "email": "test1@mail.com",
+            "rôle": "gestion"
+        }
+
+        infos = read_token()
+
+
+        mock_open_file.assert_called_once_with(".jwt_token", 'r')
+
+        mock_jwt_decode.assert_called_once()
+
+
+        self.assertIsNotNone(infos)
+        self.assertEqual(infos['email'], "test1@mail.com")
+        self.assertEqual(infos['rôle'], "gestion")
+
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
